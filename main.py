5df5dce428bb4990bee26e38ab2e701cc2057f6c
SmartsvXD <simone.vinciguerra@gmail.com>
2024-10-12 15:36:40 +0200

    style redo

--- main.py
+++ main.py
@@ -1,63 +1,80 @@
 import atexit
+import json
 
 try:
     from colorama import Style, Fore
-except:
+except ImportError:
     print('\n\n"colorama" is nedded for the operation of this code!')
-    print("Please install it by running: \"pip install colorama\"\n\n")
+    print('Please install it by running: "pip install colorama"\n\n')
     exit()
 
-from utils import *
+from utils import error, info, printColored, clearCLI
 
-#Resets text color and style when the code exits
+# Resets text color and style when the code exits
 atexit.register(lambda: print(Style.RESET_ALL))
+
 
 def loadJSONs():
     success = True
     followers, followings, whitelist = None, None, None
 
-    #Open followers and following jsons.
+    # Open followers and following jsons.
     try:
         with open("./followers_1.json") as file:
-            followers = list(follower["string_list_data"][0]['value'] for follower in json.load(file)) 
+            data = json.load(file)
+        followers = list(
+            (follower["string_list_data"][0]["value"] for follower in data)
+        )
     except FileNotFoundError:
-        error("You need to have \"followers_1.json\" on the same folder as the executable. For more, read the README.")
+        error(
+            'You need to have "followers_1.json" on the same folder as the executable. For more, read the README.'
+        )
         success = False
     except json.JSONDecodeError:
-        error("The file \"followers_1.json\" is not formatted correctly.")
+        error('The file "followers_1.json" is not formatted correctly.')
         success = False
 
     try:
         with open("./following.json") as file:
-            followings = list(following["string_list_data"][0]['value'] for following in json.load(file)['relationships_following'])
+            followings = list(
+                following["string_list_data"][0]["value"]
+                for following in json.load(file)["relationships_following"]
+            )
     except FileNotFoundError:
-        error("You need to have \"following.json\" on the same folder as the executable. For more, read the README.")
+        error(
+            'You need to have "following.json" on the same folder as the executable. For more, read the README.'
+        )
         success = False
     except json.JSONDecodeError:
-        error("The file \"following.json\" is not formatted correctly.")
+        error('The file "following.json" is not formatted correctly.')
         success = False
 
-    #Open or create the whitelist json
+    # Open or create the whitelist json
     try:
         with open("./whitelist.json") as file:
             data = json.load(file)
-            if type(data) != list:
-                error("The file \"whitelist.json\" is not formatted correctly. It should be a list.")
+            if data is list:
+                error(
+                    'The file "whitelist.json" is not formatted correctly. It should be a list.'
+                )
                 success = False
             else:
                 whitelist = list(tag for tag in data)
     except FileNotFoundError:
         with open("./whitelist.json", "w") as file:
-            file.write("[\n\t\"INSERT_HERE_THE_ACCOUNTS_YOU_DON'T_WANNA_CHECK_ON\"\n]")        
-        info("whitelist.json has been created. Write in \"whitelist.json\" the accounts you don't wanna check on.\n")
+            file.write('[\n\t"INSERT_HERE_THE_ACCOUNTS_YOU_DON\'T_WANNA_CHECK_ON"\n]')
+        info(
+            'whitelist.json has been created. Write in "whitelist.json" the accounts you don\'t wanna check on.\n'
+        )
         with open("./whitelist.json") as file:
             whitelist = list(tag for tag in json.load(file))
     except json.JSONDecodeError as e:
         print(e)
-        error("The file \"whitelist.json\" is not formatted correctly.")
+        error('The file "whitelist.json" is not formatted correctly.')
         success = False
 
     return followers, followings, whitelist, success
+
 
 def compareLists():
     followers, followings, whitelist, success = loadJSONs()
@@ -65,60 +82,75 @@ def compareLists():
     if not success:
         return
 
-    #Compare lists
+    # Compare lists
     nUnfollorers = 0
     for following in followings:
-        if following not in followers and following.lower() not in tuple(user.lower() for user in whitelist):
+        if following not in followers and following.lower() not in tuple(
+            user.lower() for user in whitelist
+        ):
             nUnfollorers += 1
-            print(f"{(len(str(len(followings)))-len(str(nUnfollorers))) * ' '}{nUnfollorers}. {Fore.RED}{Style.BRIGHT}{following.upper()}{Style.RESET_ALL} {(len(max(followings))+2-len(following)) * ' '} doesn't follow you back.")
+            print(
+                f"""{(len(str(len(followings)))-len(str(nUnfollorers))) * ' '}{nUnfollorers}. {Fore.RED}{Style.BRIGHT}
+                {following.upper()}{Style.RESET_ALL} {(len(max(followings))+2-len(following)) * ' '}
+                doesn't follow you back."""
+            )
 
     if nUnfollorers == 0:
         printColored("Congotulations! Everybody follows you back.", Fore.GREEN)
+
 
 def printWhitelist():
     whitelist, success = loadJSONs()[2:]
 
     if not success:
         return
-    
+
     if "INSERT_HERE_THE_ACCOUNTS_YOU_DON'T_WANNA_CHECK_ON" in whitelist:
         whitelist.remove("INSERT_HERE_THE_ACCOUNTS_YOU_DON'T_WANNA_CHECK_ON")
-    
+
     if len(whitelist) == 0:
         info("The whitelist is empty")
     else:
         for i in whitelist:
             print(f" - {Fore.GREEN}{Style.BRIGHT}{i}{Style.RESET_ALL}")
 
+
 def about():
-    printColored("Code written by Smartsv (SmartsvXD on Github)\n\nItaly, 2024", Fore.GREEN)
+    printColored(
+        "Code written by Smartsv (SmartsvXD on Github)\n\nItaly, 2024", Fore.GREEN
+    )
+
 
 actions = {
     "Find unfollowers": compareLists,
     "Print whitelist": printWhitelist,
-    "About": about, 
-    f"{Fore.RED}Exit{Fore.RESET}": exit
+    "About": about,
+    f"{Fore.RED}Exit{Fore.RESET}": exit,
 }
 
 prevInputInvalid = 0
 while 1:
     clearCLI()
 
-    #Print all the actions
+    # Print all the actions
     for n, i in enumerate(actions.keys()):
         print(f" {n+1}. {Style.BRIGHT}{i}{Style.RESET_ALL}")
     print()
 
-    #Print something if the previrous input was not valid
+    # Print something if the previrous input was not valid
     if prevInputInvalid:
         printColored("Invalid input!", Fore.RED)
         prevInputInvalid = 0
 
-    #Ask and run user input
+    # Ask and run user input
     try:
-        selection = int(input(f"{Fore.YELLOW}Select what you wanna do: {Fore.RESET}"))-1
+        selection = (
+            int(input(f"{Fore.YELLOW}Select what you wanna do: {Fore.RESET}")) - 1
+        )
         clearCLI()
         tuple(actions.values())[selection]()
-        input(f"{Fore.YELLOW}{Style.DIM}\n(Press ENTER to go back to menu){Style.RESET_ALL}")
-    except (IndexError, ValueError) as e:
+        input(
+            f"{Fore.YELLOW}{Style.DIM}\n(Press ENTER to go back to menu){Style.RESET_ALL}"
+        )
+    except (IndexError, ValueError):
         prevInputInvalid = 1
--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
 from colorama import Style, Fore
-import json, os
+import os
+
 
 def clearCLI():
     os.system("clear")
 
+
 def error(txt):
     print(f"{Fore.RED}{Style.BRIGHT}ERROR: {txt}")
 
+
 def info(txt):
-    print(f'{Fore.CYAN}{Style.BRIGHT}INFO: {Style.RESET_ALL}{Fore.CYAN}{txt}{Fore.RESET}')
+    print(
+        f"{Fore.CYAN}{Style.BRIGHT}INFO: {Style.RESET_ALL}{Fore.CYAN}{txt}{Fore.RESET}"
+    )
+
 
 def printColored(txt, color):
     print(f"{color}{txt}{Fore.RESET}")
