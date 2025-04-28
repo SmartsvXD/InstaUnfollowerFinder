65d1924eeebca82ef5c5a40fd886273cec19ed54
SmartsvXD <simone.vinciguerra@gmail.com>
2025-04-28 17:34:15 +0200

    fix: improve DMG creation process and enhance error handling in whitelist loading

--- utils.py
+++ utils.py
@@ -92,12 +92,9 @@ def loadWhitelist(infoF=info, errorF=error):
             whitelist = list(tag for tag in json.load(file))
     except json.JSONDecodeError as e:
         # If "whitelist.json" is not formatted correctly return an error
-        print(e)
+        errorF(e)
         errorF('The file "whitelist.json" is not formatted correctly.')
         return []
-
-    if "INSERT_HERE_THE_ACCOUNTS_YOU_DON'T_WANNA_CHECK_ON" in whitelist:
-        whitelist.remove("INSERT_HERE_THE_ACCOUNTS_YOU_DON'T_WANNA_CHECK_ON")
 
     return whitelist
 
