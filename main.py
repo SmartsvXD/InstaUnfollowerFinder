96d20c079a20756af3cf4247741f6c9071ab56c4
SmartsvXD <simone.vinciguerra@gmail.com>
2025-05-07 12:34:33 +0200

    README.md update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser("main")
 parser.add_argument(
-    "-t", "--terminal", help="Launches the app in terminal mode.", type=bool
+    "-t", "--terminal", help="Launches the app in terminal mode.",  action="store_true"
 )
 
 args = parser.parse_args()
